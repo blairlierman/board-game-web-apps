d0d9cd49bfabef9106ab9a0ef9de226db065937c
Blair Lierman <blairlierman@gmail.com>
2022-10-24 22:31:48 -0700

    feat(sff-tracker): Added MatFormField Number Input

--- apps/sff-tracker/src/app/pages/home/home.module.ts
+++ apps/sff-tracker/src/app/pages/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
 })
