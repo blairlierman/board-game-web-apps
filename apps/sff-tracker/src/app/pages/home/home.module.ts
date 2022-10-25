d74e41012186a320a20b2378ebbcdcc5171548e5
Blair Lierman <blairlierman@gmail.com>
2022-10-24 22:56:09 -0700

    fix(sff-tracker): Show Material Number Input.
    Import MatInputModule.

--- apps/sff-tracker/src/app/pages/home/home.module.ts
+++ apps/sff-tracker/src/app/pages/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [CommonModule, MatFormFieldModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule ],
   declarations: [HomeComponent],
   exports: [HomeComponent],
 })
