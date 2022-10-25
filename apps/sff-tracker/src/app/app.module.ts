9eadd35db1b1ee6838e75dd99d37dcb3cac3798a
Blair Lierman <blairlierman@gmail.com>
2022-10-24 21:40:30 -0700

    feat(sff-tracker): Add new Home Page and Route

--- apps/sff-tracker/src/app/app.module.ts
+++ apps/sff-tracker/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { HomeComponentModule } from './pages/home/home.module';
+
+const routes: Routes = [{ path: '**', component: HomeComponent }];
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    HomeComponentModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
