a0645c7c9130499a3a52341f970807d44c189f0c
Blair Lierman <blairlierman@gmail.com>
2022-10-24 22:32:48 -0700

    fix(sff-tracker): Remove Nx Welcome Component

--- apps/sff-tracker/src/app/app.module.ts
+++ apps/sff-tracker/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeComponentModule } from './pages/home/home.module';
@@ -11,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 const routes: Routes = [{ path: '**', component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent ],
   imports: [
     BrowserModule,
     HomeComponentModule,
