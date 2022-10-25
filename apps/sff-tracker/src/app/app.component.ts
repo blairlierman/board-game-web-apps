a220d245a9cde20bd28862ef89f00bacacb2c38b
Blair Lierman <blairlierman@gmail.com>
2022-10-24 20:58:28 -0700

    feat(sff-tracker): Add new SolForge Fusion app

---
+++ apps/sff-tracker/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'farming-game-remote-host-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sff-tracker';
+}
